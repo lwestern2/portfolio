9b8ce03ccc711e06bf3e87c670717268ea2347b7
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2018-12-16 23:05:37 -0700

    portfolio upload

---
+++ challenge2/project1/functions.js
@@ -0,0 +1,52 @@
+const enterButton = document.getElementById("enter");
+const input = document.getElementById("userInput");
+const ul = document.querySelector("ul");
+const item = document.getElementsByTagName("li");
+
+function inputLength() {
+    return input.value.length;
+}
+
+function listLength() {
+    return item.length;
+}
+
+function createListElement() {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+
+    function crossOut() {
+        li.classList.toggle("done");
+    }
+
+    li.addEventListener("touchend", crossOut);
+
+    const dBtn = document.createElement("button");
+    dBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(dBtn);
+    dBtn.addEventListener("touchend", deleteListItem);
+
+    function deleteListItem() {
+        li.classList.add("delete")
+    }
+}
+
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
+    }
+}
+
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.which === 13) { 
+        createListElement();
+    }
+}
+
+enterButton.addEventListener("touchend", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
